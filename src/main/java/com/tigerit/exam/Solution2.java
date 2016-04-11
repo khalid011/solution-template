8d44e581237905ab3c4a1f187cc9d2cd6c13353a
Khalid <rahmankhalid29@gmail.com>
2016-04-11 13:43:31 +0600

    added

---
+++ src/main/java/com/tigerit/exam/Solution2.java
@@ -0,0 +1,26 @@
+package com.tigerit.exam;
+
+
+import static com.tigerit.exam.IO.*;
+
+/**
+ * All of your application logic should be placed inside this class.
+ * Remember we will load your application from our custom container.
+ * You may add private method inside this class but, make sure your
+ * application's execution points start from inside run method.
+ */
+public class Solution2 implements Runnable {
+    @Override
+    public void run() {
+        // your application entry point
+
+        // sample input process
+        String string = readLine();
+
+        Integer integer = readLineAsInteger();
+
+        // sample output process
+        printLine(string);
+        printLine(integer);
+    }
+}
